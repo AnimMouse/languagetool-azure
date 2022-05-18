2493aaea36192a56d18ae68964fc7eff50502371
AnimMouse <git@animmouse.com>
2022-05-18 13:01:34 +0800

    Build ngrams on image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:19-alpine
+RUN mkdir ngrams && cd ngrams && wget -O - https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip | jar -x
 ENV VERSION 5.7
 RUN wget https://languagetool.org/download/LanguageTool-$VERSION.zip && unzip LanguageTool-$VERSION.zip && rm LanguageTool-$VERSION.zip
 WORKDIR /LanguageTool-$VERSION
--- languagetool.sh
+++ languagetool.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-mkdir /tmp/ngrams/ /tmp/ngrams-tmp/
-(wget https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip -P /tmp/ && cd /tmp/ngrams-tmp/ && jar -xvf /tmp/ngrams-en-20150817.zip && mv /tmp/ngrams-tmp/* /tmp/ngrams/ && rm -r /tmp/ngrams-en-20150817.zip /tmp/ngrams-tmp/ && echo Ngram has been set up) &
-java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port 8080 --public --allow-origin '*' --languageModel /tmp/ngrams/
+java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port 8080 --public --allow-origin '*' --languageModel /ngrams/
